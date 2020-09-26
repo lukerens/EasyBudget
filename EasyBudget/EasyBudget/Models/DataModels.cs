ff482f2cb1ed12d6fd8008f611cfda212ab3cc06
Luke Rens <lucasrens.lr@gmail.com>
2020-09-25 23:09:02 -0500

    Updating homepage

---
+++ EasyBudget/EasyBudget/Models/DataModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyBudget.Models;
+
+namespace EasyBudget.Models
+{
+    public class Expense
+    {
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public ExpenseTypes Categorie { get; set; }
+    }
+}
--- EasyBudget/EasyBudget/ViewModels/MainPageViewModel.cs
+++ EasyBudget/EasyBudget/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using EasyBudget.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -10,10 +11,62 @@ namespace EasyBudget.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService)
-            : base(navigationService)
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Main Page";
         }
+
+        #region Lifecycle
+
+
+        #endregion
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+
+            Expense expense1 = new Expense() {Price = 1, Categorie = ExpenseTypes.Gas, Date = DateTime.Now, Name = "Gas Station"};
+            Expense expense2 = new Expense() {Price = 1, Categorie = ExpenseTypes.Gas, Date = DateTime.Now, Name = "Gas Station"};
+            Expense expense3 = new Expense() {Price = 1, Categorie = ExpenseTypes.Gas, Date = DateTime.Now, Name = "Gas Station"};
+            Expense expense4 = new Expense() {Price = 1, Categorie = ExpenseTypes.Gas, Date = DateTime.Now, Name = "Gas Station"};
+            Expense expense5 = new Expense() {Price = 1, Categorie = ExpenseTypes.Gas, Date = DateTime.Now, Name = "Gas Station"};
+
+            ListOfExpenses = new List<Expense>();
+
+            ListOfExpenses.Add(expense1);
+            ListOfExpenses.Add(expense2);
+            ListOfExpenses.Add(expense3);
+            ListOfExpenses.Add(expense4);
+            ListOfExpenses.Add(expense5);
+
+            
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+        }
+
+        public override void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            base.OnNavigatedFrom(parameters);
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+        }
+
+        #region Proprities
+
+        private List<Expense> _ListOfExpenses;
+        public List<Expense> ListOfExpenses
+        {
+            get => _ListOfExpenses;
+            set => SetProperty(ref _ListOfExpenses, value);
+        }
+
+        #endregion
+
     }
 }
